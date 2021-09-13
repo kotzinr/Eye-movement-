a030d316af61357812fef4a402ad5d11f676eeef
Roxy Kotzin <86859421+kotzinr@users.noreply.github.com>
2021-09-12 17:44:25 -0700

    Add files via upload

--- eyes.js
+++ eyes.js
@@ -1,16 +1,11 @@
 const balls = document.getElementsByClassName('ball');
-
 document.onmousemove = (event) => {
-    const x = (event.clientX * 100) / window.innerWidth + '%';
-    const y = (event.clientY * 100) / window.innerHeight + '%';
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
 
-    for (let i = 0; i < 2; i++) {
-        balls[i].style.left = x;
-        balls[i].style.top = y;
-        balls[i].transform = "translate(-" + x + ",-" + y + ")";
-    }
-    balls[0].style.left = x;
-    balls[0].style.top = y;
-    balls[0].transform = 'translate(-' + x + ',-' + y + ')';
-
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
 };
